c91007ed3971012374853d99bff2a28608316e3b
Anuradha Bhatt <anudave21@gmail.com>
2017-03-05 14:11:07 -0600

    Modify values outside an array

---
+++ modify_values.rb
@@ -0,0 +1,18 @@
+# Modify values outside of the array to print : 
+# Ed Sheeran : "Shape of you" 
+# The Neighborhood : "Sweater Weather"
+# Enrique Iglesias : "Bailando"
+
+songs = ["Shape of you", "Sweater Weather", "Bailando"]
+
+songs.each do |song|
+  p "Ed Sheeran : " + " " + songs[0]
+  p "The Neighborhood : " + " " + songs[1]
+  p "Enrique Iglesias : " + " " + songs[2]
+  break
+   
+end
+
+
+
+
